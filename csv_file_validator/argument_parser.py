93630fb0716926da8ac815ddc94fefecc240cf44
Gregorio Honorato <greghono@gmail.com>
2022-12-05 18:40:40 -0300

    fix: adjust in .gitignore
    - adjust in struct __main__

--- csv_file_validator/argument_parser.py
+++ csv_file_validator/argument_parser.py
@@ -3,12 +3,51 @@ argument parser
 """
 import json
 import os
+from typing import List
 from argparse import ArgumentParser
 
 from exceptions import (
     InvalidFileLocationException,
     InvalidConfigException,
 )
+
+
+def parse_file_loc(file_loc) -> List:
+    parsed_file_loc_list = []
+    if os.path.isdir(file_loc):
+        for path in os.listdir(file_loc):
+            full_path = os.path.join(file_loc, path)
+            if os.path.isfile(full_path):
+                parsed_file_loc_list.append(full_path)
+        if not parsed_file_loc_list:
+            raise InvalidFileLocationException(f"Folder {file_loc} is empty")
+
+    elif os.path.isfile(file_loc):
+        parsed_file_loc_list = [file_loc]
+    else:
+        raise InvalidFileLocationException(
+            f"Could not load file(s) {file_loc} " f"for validation"
+        )
+    return parsed_file_loc_list
+
+
+def parse_config(config) -> List:
+    if os.path.isfile(config):
+        with open(config, mode="r") as json_file:
+            try:
+                parsed_config_dict = json.load(json_file)
+            except json.JSONDecodeError as json_decode_err:
+                raise InvalidConfigException(
+                    f"Could not load config - valid file, "
+                    f"JSON decode error: {json_decode_err}"
+                )
+            except Exception as exc:
+                raise InvalidConfigException(
+                    f"Could not load config - valid file, " f"general exception: {exc}"
+                )
+    else:
+        raise InvalidConfigException("Could not load config file - not a valid file")    
+    return parsed_config_dict
 
 
 def prepare_args() -> dict:
@@ -24,43 +63,9 @@ def prepare_args() -> dict:
     parsed = parser.parse_args()
 
     parsed_file_loc = parsed.filelocation
-    parsed_file_loc_list = []
-
-    if os.path.isdir(parsed_file_loc):
-        for path in os.listdir(parsed_file_loc):
-            full_path = os.path.join(parsed_file_loc, path)
-            if os.path.isfile(full_path):
-                parsed_file_loc_list.append(full_path)
-        if not parsed_file_loc_list:
-            raise InvalidFileLocationException(f"Folder {parsed_file_loc} is empty")
-
-    elif os.path.isfile(parsed_file_loc):
-        parsed_file_loc_list = [parsed_file_loc]
-    else:
-        raise InvalidFileLocationException(
-            f"Could not load file(s) {parsed_file_loc} " f"for validation"
-        )
-    args["file_loc"] = parsed_file_loc_list
-
     parsed_config = parsed.configfile
-    parsed_config_dict = None
-
-    if os.path.isfile(parsed_config):
-        with open(parsed_config, mode="r") as json_file:
-            try:
-                parsed_config_dict = json.load(json_file)
-            except json.JSONDecodeError as json_decode_err:
-                raise InvalidConfigException(
-                    f"Could not load config - valid file, "
-                    f"JSON decode error: {json_decode_err}"
-                )
-            except Exception as exc:
-                raise InvalidConfigException(
-                    f"Could not load config - valid file, " f"general exception: {exc}"
-                )
-    else:
-        raise InvalidConfigException("Could not load config file - not a valid file")
-
-    args["config"] = parsed_config_dict
+    
+    args["file_loc"]: List = parse_file_loc(parsed_file_loc)
+    args["config"]: List = parse_config(parsed_config)
 
     return args
